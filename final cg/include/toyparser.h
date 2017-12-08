97bb948bbb5b0026162eee61dd436502fe3d2d28
Davi Tabosa <davitabosa12@gmail.com>
2017-12-08 15:02:16 -0300

    toy parser

--- final cg/include/toyparser.h
+++ final cg/include/toyparser.h
@@ -16,7 +16,15 @@ class ToyParser {
 public:
 	ToyParser();
 	~ToyParser();
-	IndexedTriangleMesh parse(std::string objPath);
+	enum ModelType{
+		MODEL_COMPLETE = 0x00,
+		MODEL_VERTEX_ONLY = 0x01,
+		MODEL_NO_NORMALS = 0x02,
+		MODEL_NO_TEXTURE = 0x04,
+		MODEL_VERTEX_NORMALS = 0x08
+
+	};
+	void parse(std::string objPath, IndexedTriangleMesh &myMesh, ModelType modelType);
 };
 
 #endif /* TOYPARSER_H_ */
--- final cg/src/toyparser.cpp
+++ final cg/src/toyparser.cpp
@@ -5,7 +5,12 @@
  *      Author: davi
  */
 
+#include <GL/gl.h>
+#include "mesh.h"
+#include <cstring>
+#include<fstream>
 #include <toyparser.h>
+
 
 ToyParser::ToyParser() {
 	// TODO Auto-generated constructor stub
@@ -16,12 +21,78 @@ ToyParser::~ToyParser() {
 	// TODO Auto-generated destructor stub
 }
 
-IndexedTriangleMesh ToyParser::parse(std::string objPath){
-
+void ToyParser::parse(std::string objPath, IndexedTriangleMesh &myMesh, ModelType modelType){
 	std::vector< unsigned int > vertexIndices, uvIndices, normalIndices;
-	std::vector< Vec3 > temp_vertices;
-	std::vector< Vec2 > temp_uvs;
-	std::vector< Vec3 > temp_normals;
-	FILE *f = fopen(objPath,"r");
+	std::vector< Vertex > temp_vertices;
+	std::vector< Vertex > temp_textures;
+	std::vector< Vertex > temp_normals;
+#if 0
+
+
+	FILE *f = fopen(objPath.c_str(),"r");
+	if(f == NULL){
+		std::cout << "Nao foi possivel abrir o modelo." << std::endl;
+		return;
+	}
+	while(1){
+		char lineHeader[128];
+
+		int res = fscanf(f,"%s",lineHeader);
+		if (res == EOF)
+			break;
+		else{
+
+		}
+	}
+#else
+	std::string line;
+	std::ifstream myfile (objPath);
+	char tipo[128];
+	if(myfile.is_open()){
+		while(getline(myfile,line)){
+			sscanf(line.c_str(),"%s",tipo);
+			if(strcmp(tipo,"v") == 0){ //as coordenadas sao de vertice
+							Vertex v;
+							sscanf(line.c_str(),"%*s %f %f %f\n", &v.x, &v.y, &v.z);
+							temp_vertices.push_back(v);
+							myMesh.addVertex(v.x,v.y,v.z);
+							//std::cout << "Vertex: x = " << v.x << " y = " << v.y << " z = " << v.z  << std::endl;
+						}
+						else if(strcmp(tipo, "vt") == 0){ //coordenadas de textura
+							Vertex vt;
+							sscanf(line.c_str(),"%*s %f %f\n", &vt.x, &vt.y);
+							temp_textures.push_back(vt);
+						}
+						else if(strcmp(tipo, "vn") == 0){ //coordenadas de normal
+							Vertex vn;
+							sscanf(line.c_str(),"%f %f %f\n", &vn.x, &vn.y, &vn.z);
+							temp_normals.push_back(vn);
+						}
+						else if(strcmp(tipo, "f") == 0){ //indice de face
+							unsigned int vIndex[3], vtIndex[3], vnIndex[3];
+							if(modelType == MODEL_NO_TEXTURE)
+							sscanf(line.c_str(),"%*s %d%*c%*c%d %d%*c%*c%d %d%*c%*c%d\n", &vIndex[0],&vnIndex[0],
+																	 	 	 	&vIndex[1],&vnIndex[1],
+																				&vIndex[2],&vnIndex[2]);
+							myMesh.addTriangle(vIndex[0],vIndex[1],vIndex[2]);
+							/*std::cout << "vIndex: " << vIndex[0] << " "<< vIndex[1] << " "<< vIndex[2] << " "
+									<<  " vnIndex: " << vnIndex[0] << " "<< vnIndex[1] << " "<< vnIndex[2] << " "
+									<<std::endl;*/
+						}
+						else if(strcmp(tipo, "#") == 0){
+
+
+							//std::cout << "Comment says: " << line << std::endl;
+						}
+						else{
+							//std::cout << "Random" << std::endl;
+						}
+		}
+		myfile.close();
+	}
+	else
+		std::cout << "Nao consegui abrir" << std::endl;
+	return;
+#endif
 
 }
